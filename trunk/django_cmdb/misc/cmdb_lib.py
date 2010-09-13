352ea4c923bd68fa90254910938bcf1e58ce090b
mozrat@gmail.com <mozrat@gmail.com@ab8b802c-fe08-a1c3-074d-5fc266e36c6e>
2010-09-13 17:39:28 +0000

    Added module documentation for cmdb_lib

--- trunk/django_cmdb/misc/cmdb_lib.py
+++ trunk/django_cmdb/misc/cmdb_lib.py
@@ -3,12 +3,19 @@ logging.basicConfig(level=logging.DEBUG)
 
 
 def get_parent_paths(path):
+    """
+    Return a list of strings that represents the parent classes of a ConfigurationItem.
+    Running it against '/Devices/Servers/SERVER001' returns
+    ['/Devices/Servers/SERVER001', '/Devices/Servers', '/Devices', '/']
+    """
+
     split_path = path.split('/')
     try:
         split_path.remove('') # Remove the empty item that is left as the first item in the list
     except ValueError:
         # Someone has entered a path with no / in it... 
         raise ValidationError(u'Invalid Path')
+
     paths = [] # Create an empty list to hold the paths
     while len(split_path) > 0:
         # For each item in the CIs path
@@ -23,39 +30,56 @@ def get_parent_paths(path):
         # Delete the part and do it all again....
     # Insert a single slash at the beginning of the list
     paths.append('/')
+    # Return something like ['/Devices/Servers/SERVER001', '/Devices/Servers', '/Devices', '/']
     return paths
 
-def get_options_for_combo(string):
-    ret = []
-    from cmdb.models import ConfigurationItem
-    try:
-        for ci in ConfigurationItem.objects.filter(active=True,
-            path__startswith=string):
-            short_path = ci.path[ len(string): ]
-            if not short_path:
-                pass
-            else:
-                ret.append( (ci.pk, short_path[1:] ) )
-        return ret
-    except:
-        pass
 
 def get_correct_class(*args, **kwargs):
+    """
+    Return a Django database object that is the correct class according to
+    the path. For example object /Devices/Servers/SERVER001 is a ConfigurationItem,
+    Device and a Server.
+
+    get_correct_class return the Server representation of this object
+
+    >>> ci = ConfigurationItem.objects.get(path='/Devices/Servers/SERVER01')
+    >>> get_correct_class(ci=ci)
+    <Server: /Devices/Servers/SERVER01>
+
+    You can also provide a string to get the correct object back
+
+    >>> get_correct_class(ci_path='/Devices/Servers/SERVER01')
+    <Server: /Devices/Servers/SERVER01>
+    """
+
+    from cmdb.models import *
     if not kwargs.has_key('ci'):
         ci = ConfigurationItem.objects.get(path=kwargs['ci_path'])
     else:
         ci = kwargs['ci']
 
-    from cmdb.models import *
     for part in get_parent_paths(ci.path):
         try:
             s = Schema.objects.get(path=part)
+            # Return an object list <Server: /Devices/Servers/SERVER01>
             return eval('''%s.objects.get(path='%s')''' % ( s.class_name, ci.path))
         except Schema.DoesNotExist:
             pass
     return False
 
 def get_schema_for_ci(*args, **kwargs):
+    """
+    Return the Schema object given either an instance
+    of a Django object or a string containing a path
+
+    >>> ci = ConfigurationItem.objects.get(path='/Devices/Servers/SERVER01')
+    >>> get_schema_for_ci(ci=ci)
+    <Schema: /Devices/Servers>
+
+    >>> get_schema_for_ci(ci_path='/Devices/Servers/SERVER01')
+    <Schema: /Devices/Servers>
+    """
+
     from cmdb.models import ConfigurationItem, Schema
     if not kwargs.has_key('ci_path'):
         ci_path = kwargs['ci'].path
@@ -65,16 +89,32 @@ def get_schema_for_ci(*args, **kwargs):
     for part in get_parent_paths(ci_path):
         try:
             s = Schema.objects.get(path=part)
+            # Return something like <Schema: /Devices/Servers>
             return s
         except Schema.DoesNotExist:
             pass
     return False
         
 def prefix_slash(s):
+    """
+    Return a string with a path prepended to it, helpful when the URL 
+    processor strips it out
+
+    >>> prefix_slash('Devices/Servers/SERVER01')
+    '/Devices/Servers/SERVER01'
+    """
     return '''/%s''' % s
 
 
 def collect_all_attributes_for_schema(schema):
+    """
+    Return a list of strings that represents the attributes associated with a Schema
+
+    >>> s = Schema.objects.get(path='/Devices/Servers')
+    >>> collect_all_attributes_for_schema(s)
+    ['id', 'path', 'name', 'description', 'is_active'..... ]
+    """
+
     from cmdb.models import *
     parent_classes = get_parent_paths(schema.path)
     fields = []
@@ -87,18 +127,5 @@ def collect_all_attributes_for_schema(schema):
             if f not in fields:
                 fields.append(f)
     return fields
-
-def serialize_object(ci):
-    import simplejson as json
-    fields = collect_all_attributes_for_schema(get_schema_for_ci(ci=ci))
-    ret = {}
-    for f in fields:
-        if f[-4:] == '_ptr':
-            pass
-        else:
-            ret[f] = eval('''ci.%s''' % f)
-            logging.debug("Serializing field %s, value %s" % ( f, ret[f] ))
-    return json.dumps(ret)
-
 
         
